f0a91e3b987afa5e9789e92dedea8183f484180c
Anastasia <goncharovaanastasia53@gmail.com>
2026-02-09 14:43:34 +0700

    tasks 6-7

--- js/main.js
+++ js/main.js
@@ -5,21 +5,34 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inventory: 100,
+        inStock: false,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        //test
-
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart +=1
+        },
+        deleteToCart() {
+            this.cart -=1
+        },
+        updateProduct(variantImage) {
+          this.image = variantImage
+        }
     }
 })
